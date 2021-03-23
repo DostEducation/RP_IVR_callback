9e93f45f192f971892369298eafebe9a1ed7045e
rudresh <rudresh.shrotriya@gmail.com>
2021-03-23 22:11:10 +0530

    pre-commit into play

--- api/services/call_log_event_service.py
+++ api/services/call_log_event_service.py
@@ -1,45 +1,46 @@
 # This file is treated as service layer
 from api import models, db
 
+
 class CallLogEventService:
     ### Main function, used to getting called from cloud function
     def handle_call_log_event(self, request):
         try:
             data = {}
-            data['call_sid'] = request.args.get('CallSid')
-            data['account_sid'] = request.args.get('AccountSid')
-            data['from_number'] = request.args.get('From')
-            data['to_number'] = request.args.get('To')
-            data['call_status'] = request.args.get('CallStatus')
-            data['direction'] = request.args.get('Direction')
-            data['parent_call_sid'] = request.args.get('ParentCallSid')
-            data['telco_code'] = request.args.get('TelcoCode')
-            data['telco_status'] = request.args.get('TelcoStatus')
-            data['dial_time'] = request.args.get('DialTime')
-            data['pick_time'] = request.args.get('PickTime')
-            data['end_time'] = request.args.get('EndTime')
-            data['duration'] = request.args.get('Duration')
+            data["call_sid"] = request.args.get("CallSid")
+            data["account_sid"] = request.args.get("AccountSid")
+            data["from_number"] = request.args.get("From")
+            data["to_number"] = request.args.get("To")
+            data["call_status"] = request.args.get("CallStatus")
+            data["direction"] = request.args.get("Direction")
+            data["parent_call_sid"] = request.args.get("ParentCallSid")
+            data["telco_code"] = request.args.get("TelcoCode")
+            data["telco_status"] = request.args.get("TelcoStatus")
+            data["dial_time"] = request.args.get("DialTime")
+            data["pick_time"] = request.args.get("PickTime")
+            data["end_time"] = request.args.get("EndTime")
+            data["duration"] = request.args.get("Duration")
             self.create_call_log_event(data)
-            
+
         except IndexError:
             print("Failed to log the call details")
 
     # Create a call log event
     def create_call_log_event(self, data):
         call_log_event = models.CallLogEvent(
-            call_sid = data['call_sid'],
-            account_sid = data['account_sid'],
-            from_number = data['from_number'],
-            to_number = data['to_number'],
-            call_status = data['call_status'],
-            direction = data['direction'],
-            parent_call_sid = data['parent_call_sid'], 
-            telco_code = data['telco_code'],
-            telco_status = data['telco_status'],
-            dial_time = data['dial_time'],
-            pick_time = data['pick_time'],
-            end_time = data['end_time'],
-            duration = data['duration'],
+            call_sid=data["call_sid"],
+            account_sid=data["account_sid"],
+            from_number=data["from_number"],
+            to_number=data["to_number"],
+            call_status=data["call_status"],
+            direction=data["direction"],
+            parent_call_sid=data["parent_call_sid"],
+            telco_code=data["telco_code"],
+            telco_status=data["telco_status"],
+            dial_time=data["dial_time"],
+            pick_time=data["pick_time"],
+            end_time=data["end_time"],
+            duration=data["duration"],
         )
         db.session.add(call_log_event)
         db.session.commit()
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 # from pprint import pprint
 from api import models, db, services
 
+
 def callback(request):
     try:
-        if(request.method == 'POST'):
+        if request.method == "POST":
             call_log_event_service = services.CallLogEventService()
             call_log_event_service.handle_call_log_event(request)
         else:
-            return jsonify(message="Currently, the system do not accept a GET request"), 405
+            return (
+                jsonify(message="Currently, the system do not accept a GET request"),
+                405,
+            )
     except IndexError:
         return jsonify(message="Something went wrong!"), 400
-    return 'Success'
+    return "Success"
