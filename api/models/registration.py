38e6e11d09ace2f379a51f4425518c1261eb6cc3
rudresh <rudresh.shrotriya@gmail.com>
2021-06-01 16:42:45 +0530

    formatting changes

--- api/models/registration.py
+++ api/models/registration.py
@@ -1,20 +1,5 @@
 from api.mixins import TimestampMixin
 from api import db
-import enum
-
-
-class ParentTypeEnum(enum.Enum):
-    FATHER = ("Father",)
-    MOTHER = ("Mother",)
-    GRANDFATHER = ("Grand Father",)
-    GRANDMOTHER = ("Grand Mother",)
-    OTHER = ("Other",)
-
-
-class RegistrationStatusEnum(enum.Enum):
-    PENDING = ("Pending",)
-    INPROGRESS = ("In-Progress",)
-    COMPLETE = ("Complete",)
 
 
 class Registration(TimestampMixin, db.Model):
@@ -27,10 +12,7 @@ class Registration(TimestampMixin, db.Model):
     program_id = db.Column(db.Integer, db.ForeignKey("program.id"))
     district = db.Column(db.String(100), nullable=True)
     state = db.Column(db.String(100), nullable=True)
-    parent_type = db.Column(
-        db.Enum(ParentTypeEnum),
-        # default = ParentTypeEnum.OTHER
-    )
+    parent_type = db.Column(db.String(100), nullable=True)
     area_type = db.Column(db.String(255), nullable=True)
     is_child_between_0_3 = db.Column(db.Boolean, nullable=True)
     is_child_between_3_6 = db.Column(db.Boolean, nullable=True)
@@ -40,8 +22,5 @@ class Registration(TimestampMixin, db.Model):
     has_smartphone = db.Column(db.Boolean, nullable=True)
     has_dropped_missedcall = db.Column(db.Boolean, nullable=True)
     has_received_callback = db.Column(db.Boolean, nullable=True)
-    status = db.Column(
-        db.Enum(RegistrationStatusEnum),
-        default=RegistrationStatusEnum.PENDING,
-    )
+    status = db.Column(db.String(100), nullable=True)
     signup_date = db.Column(db.DateTime, nullable=True)
