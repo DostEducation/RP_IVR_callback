4c24c8c72d89307da53a6d49e818021dd3b8b22f
Gaurav Gusain <gaurav.gusain@coloredcow.in>
2022-08-18 13:37:13 +0530

    Added new column in registration table

--- api/models/registration.py
+++ api/models/registration.py
@@ -16,6 +16,7 @@ class Registration(TimestampMixin, db.Model):
     education_level = db.Column(db.String(255), nullable=True)
     occupation = db.Column(db.String(255), nullable=True)
     gender_of_child = db.Column(db.String(255), nullable=True)
+    number_of_eligible_kids = db.Column(db.String(255), nullable=True)
     area_type = db.Column(db.String(255), nullable=True)
     is_child_between_0_3 = db.Column(db.Boolean, nullable=True)
     is_child_between_3_6 = db.Column(db.Boolean, nullable=True)
